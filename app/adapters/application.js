ee088af3f4b5173c23200467b2b70be7d3389d66
ascarano <adamscarano@gmail.com>
2015-02-04 13:01:12 -0700

    updated cli and re-added packages

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from "ember-data";
 
-export default DS.LSAdapter.extend({});
+// export default DS.LSAdapter.extend({});
 
 export default DS.ActiveModelAdapter.extend({
   host: 'http://localhost:3000'
